42f3b44aa7753b7e8f4291225e2c115f877ddf49
Breno Cortês Da Rosa <71451784+Akunozz@users.noreply.github.com>
2025-06-17 11:38:59 -0300

    feat: login com google

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,9 +2,17 @@
 import type { Config } from 'tailwindcss'
 
 const config: Config = {
-  content: ['./src/**/*.{js,jsx,ts,tsx,html}'],
+  content: [
+    './src/**/*.{js,jsx,ts,tsx,html}',
+    './src/app/**/*.{js,jsx,ts,tsx,html}',
+    './src/components/**/*.{js,jsx,ts,tsx,html}',
+    './src/pages/**/*.{js,jsx,ts,tsx,html}',
+    './src/**/*.{mdx}',
+  ],
   theme: { /* ... */ },
   plugins: [ /* ... */ ],
 }
 
 export default config
+// Para máxima compatibilidade com o IntelliSense do Tailwind CSS, adicione também:
+// module.exports = config
